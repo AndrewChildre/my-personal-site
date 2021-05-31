3b5003cdd5f9a7018eadceeb1eae66054783be88
andrew childre <andrewchildre@gmail.com>
2021-05-30 21:44:16 -0600

    working background

--- src/components/SkillSet.js
+++ src/components/SkillSet.js
@@ -1,146 +1,363 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-const SkillSet = () => {
-    return (
-        <div  >
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	paper: {
+		padding: theme.spacing(2),
+		textAlign: 'center',
+		// color: theme.palette.text.secondary,
+		backgroundColor: '#5199e4'
+	},
+	headings: {
+		fontFamily: 'Caveat',
+		fontSize: 50
 
-                                <h2>Skill Set</h2>
-						
-						<a  href='https://getbootstrap.com' target='_blank' rel='noreferrer'>
-							{' '}
-							<img 
-								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg'
-								alt='bootstrap'
+	}
+}));
+
+export default function CenteredGrid() {
+	const classes = useStyles();
+
+	return (
+		<div className={classes.root}>
+			<Grid className='skills' container spacing={3}>
+				<Grid item xs={4}>
+					<h1 className={classes.headings}>Frontend</h1>
+					<Paper className={classes.paper}>
+					
+						<a href='https://www.w3.org/html/' target='_blank' rel='noreferrer'>
+							<img
+								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg'
+								alt='html5'
 								width='70'
 								height='70'
 								
 							/>
-						</a>{' '}
-					
+						</a>
 						<a
 							href='https://www.w3schools.com/css/'
 							target='_blank'
 							rel='noreferrer'>
-							{' '}
+					
 							<img
 								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg'
 								alt='css3'
 								width='70'
 								height='70'
 							/>
-						</a>{' '}
-						<a
-							href='https://www.djangoproject.com/'
-							target='_blank'
-							rel='noreferrer'>
-							{' '}
-							<img
-								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/django/django-original.svg'
-								alt='django'
-								width='70'
-								height='70'
-							/>
-						</a>{' '}
-						<a href='https://expressjs.com' target='_blank' rel='noreferrer'>
-							{' '}
-							<img
-								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original.svg'
-								alt='express'
-								width='70'
-								height='70'
-							/>
-						</a>{' '}
-						<a href='https://git-scm.com/' target='_blank' rel='noreferrer'>
-							{' '}
-							<img
-								src='https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg'
-								alt='git'
-								width='70'
-								height='70'
-							/>
-						</a>{' '}
-						<a href='https://heroku.com' target='_blank' rel='noreferrer'>
-							{' '}
-							<img
-								src='https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg'
-								alt='heroku'
-								width='70'
-								height='70'
-							/>
-						</a>{' '}
-						<a href='https://www.w3.org/html/' target='_blank' rel='noreferrer'>
-							{' '}
-							<img
-								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg'
-								alt='html5'
-								width='70'
-								height='70'
-							/>
-						</a>{' '}
-						<a
+						</a>
+							<a
 							href='https://developer.mozilla.org/en-US/docs/Web/JavaScript'
 							target='_blank'
 							rel='noreferrer'>
-							{' '}
+							
 							<img
 								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg'
 								alt='javascript'
 								width='70'
 								height='70'
 							/>
-						</a>{' '}
-				
-						<a href='https://www.mongodb.com/' target='_blank' rel='noreferrer'>
-							{' '}
+						</a>
+							<a href='https://reactjs.org/' target='_blank' rel='noreferrer'>
+						
 							<img
-								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg'
-								alt='mongodb'
+								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg'
+								alt='react'
 								width='70'
 								height='70'
 							/>
-						</a>{' '}
-				
-						<a href='https://nodejs.org' target='_blank' rel='noreferrer'>
-							{' '}
+						</a>
+						<a href='https://getbootstrap.com' target='_blank' rel='noreferrer'>
+							
 							<img
-								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg'
-								alt='nodejs'
+								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg'
+								alt='bootstrap'
 								width='70'
 								height='70'
 							/>
-						</a>{' '}
-						<a
-							href='https://www.postgresql.org'
-							target='_blank'
-							rel='noreferrer'>
-							{' '}
+						</a>
+										<a href='https://material-ui.com/' target='_blank' rel='noreferrer'>
+						
 							<img
-								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg'
-								alt='postgresql'
+								src='https://raw.githubusercontent.com/AndrewChildre/devicon/master/icons/materialui/materialui-original.svg'
+								alt='vs code'
 								width='70'
 								height='70'
 							/>
-						</a>{' '}
-						<a href='https://www.python.org' target='_blank' rel='noreferrer'>
-							{' '}
+						</a>
+						
+					</Paper>
+				</Grid>
+				<Grid item xs={4}>
+					<h1 className={classes.headings}>Backend</h1>
+					<Paper className={classes.paper}>
+								<a href='https://www.python.org' target='_blank' rel='noreferrer'>
+							
 							<img
 								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg'
 								alt='python'
 								width='70'
 								height='70'
 							/>
-						</a>{' '}
-						<a href='https://reactjs.org/' target='_blank' rel='noreferrer'>
-							{' '}
+						</a>
+						<a
+							href='https://www.djangoproject.com/'
+							target='_blank'
+							rel='noreferrer'>
+							
 							<img
-								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg'
-								alt='react'
+								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/django/django-original.svg'
+								alt='django'
 								width='70'
 								height='70'
-							/>{' '}
+							/>
 						</a>
-        </div>
-    );
-};
+						
+					
 
-export default SkillSet;
+					
+						<a href='https://www.mongodb.com/' target='_blank' rel='noreferrer'>
+			
+							<img
+								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg'
+								alt='mongodb'
+								width='70'
+								height='70'
+							/>
+						</a>
+							<a href='https://expressjs.com' target='_blank' rel='noreferrer'>
+							
+							<img
+								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original.svg'
+								alt='express'
+								width='70'
+								height='70'
+							/>
+						</a>
+							<a
+							href='https://www.postgresql.org'
+							target='_blank'
+							rel='noreferrer'>
+						
+							<img
+								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg'
+								alt='postgresql'
+								width='70'
+								height='70'
+							/>
+						</a>
+					</Paper>
+				</Grid>
+				<Grid item xs={4}>
+					<h1 className={classes.headings}>Other</h1>
+					<Paper className={classes.paper}>
+						<a href='https://git-scm.com/' target='_blank' rel='noreferrer'>
+						
+							<img
+								src='https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg'
+								alt='git'
+								width='70'
+								height='70'
+							/>
+						</a>
+					
+						<a href='https://nodejs.org' target='_blank' rel='noreferrer'>
+							
+							<img
+								src='https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg'
+								alt='nodejs'
+								width='70'
+								height='70'
+							/>
+						</a>
+							<a href='https://heroku.com' target='_blank' rel='noreferrer'>
+						
+							<img
+								src='https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg'
+								alt='heroku'
+								width='70'
+								height='70'
+							/>
+						</a>
+							<a href='https://code.visualstudio.com/' target='_blank' rel='noreferrer'>
+						
+							<img
+								src='https://raw.githubusercontent.com/AndrewChildre/devicon/master/icons/visualstudio/visualstudio-plain.svg'
+								alt='vs code'
+								width='70'
+								height='70'
+							/>
+						</a>
+								<a href='https://github.com/' target='_blank' rel='noreferrer'>
+						
+							<img
+								src='https://raw.githubusercontent.com/AndrewChildre/devicon/master/icons/github/github-original-wordmark.svg'
+								alt='vs code'
+								width='70'
+								height='70'
+							/>
+						</a>
+									<a href='https://slack.com/' target='_blank' rel='noreferrer'>
+						
+							<img
+								src='https://raw.githubusercontent.com/AndrewChildre/devicon/master/icons/slack/slack-original-wordmark.svg'
+								alt='vs code'
+								width='70'
+								height='70'
+							/>
+						</a>
+				
+					
+				
+					
+					</Paper>
+				</Grid>
+			</Grid>
+		</div>
+	);
+}
+
+// import React from 'react';
+// import '../App.css'
+// const SkillSet = () => {
+//     return (
+// 			<div className='skills'>
+// 				<h2>Skill Set</h2>
+// 				<div className='frontend'>
+// 				<a href='https://getbootstrap.com' target='_blank' rel='noreferrer'>
+// 					{' '}
+// 					<img
+// 						src='https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg'
+// 						alt='bootstrap'
+// 						width='70'
+// 						height='70'
+// 					/>
+// 				</a>{' '}
+// 				<a
+// 					href='https://www.w3schools.com/css/'
+// 					target='_blank'
+// 					rel='noreferrer'>
+// 					{' '}
+// 					<img
+// 						src='https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg'
+// 						alt='css3'
+// 						width='70'
+// 						height='70'
+// 					/>
+// 				</a>{' '}
+// 				<a
+// 					href='https://www.djangoproject.com/'
+// 					target='_blank'
+// 					rel='noreferrer'>
+// 					{' '}
+// 					<img
+// 						src='https://raw.githubusercontent.com/devicons/devicon/master/icons/django/django-original.svg'
+// 						alt='django'
+// 						width='70'
+// 						height='70'
+// 					/>
+// 				</a>{' '}
+// 				</div>
+// 				<a href='https://expressjs.com' target='_blank' rel='noreferrer'>
+// 					{' '}
+// 					<img
+// 						src='https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original.svg'
+// 						alt='express'
+// 						width='70'
+// 						height='70'
+// 					/>
+// 				</a>{' '}
+// 				<a href='https://git-scm.com/' target='_blank' rel='noreferrer'>
+// 					{' '}
+// 					<img
+// 						src='https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg'
+// 						alt='git'
+// 						width='70'
+// 						height='70'
+// 					/>
+// 				</a>{' '}
+// 				<a href='https://heroku.com' target='_blank' rel='noreferrer'>
+// 					{' '}
+// 					<img
+// 						src='https://www.vectorlogo.zone/logos/heroku/heroku-icon.svg'
+// 						alt='heroku'
+// 						width='70'
+// 						height='70'
+// 					/>
+// 				</a>{' '}
+// 				<a href='https://www.w3.org/html/' target='_blank' rel='noreferrer'>
+// 					{' '}
+// 					<img
+// 						src='https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg'
+// 						alt='html5'
+// 						width='70'
+// 						height='70'
+// 					/>
+// 				</a>{' '}
+// 				<a
+// 					href='https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+// 					target='_blank'
+// 					rel='noreferrer'>
+// 					{' '}
+// 					<img
+// 						src='https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg'
+// 						alt='javascript'
+// 						width='70'
+// 						height='70'
+// 					/>
+// 				</a>{' '}
+// 				<a href='https://www.mongodb.com/' target='_blank' rel='noreferrer'>
+// 					{' '}
+// 					<img
+// 						src='https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg'
+// 						alt='mongodb'
+// 						width='70'
+// 						height='70'
+// 					/>
+// 				</a>{' '}
+// 				<a href='https://nodejs.org' target='_blank' rel='noreferrer'>
+// 					{' '}
+// 					<img
+// 						src='https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg'
+// 						alt='nodejs'
+// 						width='70'
+// 						height='70'
+// 					/>
+// 				</a>{' '}
+// 				<a href='https://www.postgresql.org' target='_blank' rel='noreferrer'>
+// 					{' '}
+// 					<img
+// 						src='https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg'
+// 						alt='postgresql'
+// 						width='70'
+// 						height='70'
+// 					/>
+// 				</a>{' '}
+// 				<a href='https://www.python.org' target='_blank' rel='noreferrer'>
+// 					{' '}
+// 					<img
+// 						src='https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg'
+// 						alt='python'
+// 						width='70'
+// 						height='70'
+// 					/>
+// 				</a>{' '}
+// 				<a href='https://reactjs.org/' target='_blank' rel='noreferrer'>
+// 					{' '}
+// 					<img
+// 						src='https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg'
+// 						alt='react'
+// 						width='70'
+// 						height='70'
+// 					/>{' '}
+// 				</a>
+// 			</div>
+// 		);
+// };
+
+// export default SkillSet;
